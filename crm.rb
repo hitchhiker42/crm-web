6a804a704c7405970543c5b5832c90e6fbe73153
Aysha Edathodu <aysha.edathodu@gmail.com>
2015-06-16 15:42:31 -0400

    Contacts class added

--- crm.rb
+++ crm.rb
@@ -1,6 +1,14 @@
+# This is like all the mathy/functional/'method' ey stuff
+require_relative 'contact'
 require 'sinatra'
 
 get '/' do
-	erb :index
+	@crm_app_name = "My CRM" # @ symbol has nothing to do with OOP. 
+	@num = 1+2 # @ symbol is preparing a variable for view
+	erb :index # tells you to send off everything above it ot index.erb
+	# ^ its also sending everything off to the client
 end
 
+get '/contacts' do 
+	erb :contacts
+end
