1a8813671addff8394f50ad9bf4b103782431044
Aysha Edathodu <aysha.edathodu@gmail.com>
2015-06-17 17:14:00 -0400

    final css + layout

--- crm.rb
+++ crm.rb
@@ -3,11 +3,11 @@ require_relative 'contact'
 require_relative 'rolodex'
 require 'sinatra'
 
-$rolodex = Rolodex.new
+$rolodex = Rolodex.new #global variable allows the server not to forget between requests
 
 get '/' do
 	@crm_app_name = "My CRM" # @ symbol has nothing to do with OOP. 
-	@num = 1+2 # @ symbol is preparing a variable for view
+	# @num = 1+2 # @ symbol is preparing a variable for view
 	erb :index # tells you to send off everything above it ot index.erb
 	# ^ its also sending everything off to the client
 end
